d8617c565b69fcd6029dea98110fc7c5e1d09e64
Madzel <stumpi@userzap.de>
2026-06-18 16:54:53 +0000

    Make ISS Tracker auto refresh always active

--- src/gui.c
+++ src/gui.c
@@ -29,8 +29,9 @@ static int in_rect(WORD mx, WORD my, WORD x, WORD y, WORD w, WORD h){ return mx>
 static void text_at(struct RastPort *rp, WORD x, WORD y, const char *s){ Move(rp,x,y); Text(rp,(STRPTR)s,strlen(s)); }
 static void set_status(IssTrackerApp *app, UBYTE st, const char *txt){ UWORD i; app->status=st; for(i=0;txt[i]&&i+1<sizeof(app->status_text);i++) app->status_text[i]=txt[i]; app->status_text[i]=0; }
 static void append_num(char *b, UWORD *p, LONG n){ char t[12]; WORD i; WORD j; if(n<0){ b[(*p)++]='-'; n=-n; } i=0; do{ t[i++]=(char)('0'+(n%10)); n/=10; }while(n&&i<11); for(j=i-1;j>=0;j--) b[(*p)++]=t[j]; b[*p]=0; }
+static UWORD parse_minutes(const char *s){ ULONG v; v=0; while(*s==' ') s++; while(*s>='0' && *s<='9'){ v=(v*10)+(*s-'0'); if(v>120) return 120; s++; } if(v<1) v=1; if(v>120) v=120; return (UWORD)v; }
 static void draw_small_button(struct RastPort *rp, WORD x, WORD y, WORD w, WORD h, const char *label){ SetAPen(rp,1); RectFill(rp,x,y,x+w,y+h); SetAPen(rp,0); Move(rp,x+6,y+11); Text(rp,(STRPTR)label,strlen(label)); SetAPen(rp,3); Move(rp,x,y); Draw(rp,x+w,y); Draw(rp,x+w,y+h); Draw(rp,x,y+h); Draw(rp,x,y); }
-static void draw_interval_window(struct Window *w, UWORD value){ struct RastPort *rp; char num[8]; UWORD p; rp=w->RPort; SetDrMd(rp,JAM1); SetAPen(rp,0); RectFill(rp,0,0,w->Width-1,w->Height-1); SetAPen(rp,1); text_at(rp,10,18,"Update interval (minutes)"); p=0; append_num(num,&p,value); SetAPen(rp,1); text_at(rp,96,40,num); draw_small_button(rp,24,55,34,16,"-"); draw_small_button(rp,66,55,34,16,"+"); draw_small_button(rp,118,55,42,16,"OK"); draw_small_button(rp,168,55,54,16,"Cancel"); }
+static void draw_interval_window(struct Window *w, struct Gadget *g){ struct RastPort *rp; rp=w->RPort; SetDrMd(rp,JAM1); SetAPen(rp,0); RectFill(rp,0,0,w->Width-1,w->Height-1); SetAPen(rp,1); text_at(rp,10,18,"Update interval minutes"); text_at(rp,10,40,"Minutes:"); SetAPen(rp,1); Move(rp,82,26); Draw(rp,132,26); Draw(rp,132,45); Draw(rp,82,45); Draw(rp,82,26); RefreshGList(g,w,0,1); draw_small_button(rp,48,58,42,16,"OK"); draw_small_button(rp,102,58,54,16,"Cancel"); }
 static void update_now(struct Window *win, IssTrackerApp *app)
 {
     IssPosition p;
@@ -123,25 +124,47 @@ static void open_interval_window(IssTrackerApp *app, struct Window *parent)
 {
     struct NewWindow nw;
     struct Window *w;
+    struct Gadget sg;
+    struct StringInfo si;
+    char buf[8];
+    char undo[8];
+    UWORD p;
     WORD done;
-    UWORD val;
+    memset(&sg,0,sizeof(sg));
+    memset(&si,0,sizeof(si));
+    memset(buf,0,sizeof(buf));
+    memset(undo,0,sizeof(undo));
+    p=0;
+    append_num(buf,&p,app->update_interval_min);
+    si.Buffer=(STRPTR)buf;
+    si.UndoBuffer=(STRPTR)undo;
+    si.MaxChars=4;
+    si.LongInt=app->update_interval_min;
+    sg.LeftEdge=86;
+    sg.TopEdge=30;
+    sg.Width=42;
+    sg.Height=12;
+    sg.Flags=GFLG_GADGHBOX;
+    sg.Activation=GACT_RELVERIFY|GACT_LONGINT;
+    sg.GadgetType=GTYP_STRGADGET;
+    sg.SpecialInfo=(APTR)&si;
     memset(&nw,0,sizeof(nw));
     nw.LeftEdge=(WORD)(parent->LeftEdge+24);
     nw.TopEdge=(WORD)(parent->TopEdge+24);
-    nw.Width=236;
-    nw.Height=86;
+    nw.Width=184;
+    nw.Height=90;
     nw.DetailPen=0;
     nw.BlockPen=1;
-    nw.IDCMPFlags=IDCMP_CLOSEWINDOW|IDCMP_MOUSEBUTTONS|IDCMP_REFRESHWINDOW;
+    nw.IDCMPFlags=IDCMP_CLOSEWINDOW|IDCMP_MOUSEBUTTONS|IDCMP_REFRESHWINDOW|IDCMP_GADGETUP;
     nw.Flags=WFLG_CLOSEGADGET|WFLG_DRAGBAR|WFLG_DEPTHGADGET|WFLG_ACTIVATE|WFLG_SMART_REFRESH;
+    nw.FirstGadget=&sg;
     nw.Type=WBENCHSCREEN;
     nw.Title=(UBYTE *)"Update Interval";
     w=OpenWindow(&nw);
     if(!w){ strcpy(app->info_text,"Interval window failed"); draw_panel(parent,app); return; }
-    val=app->update_interval_min;
-    draw_interval_window(w,val);
+    draw_interval_window(w,&sg);
     done=0;
-    while(!done){ ULONG sig; sig=Wait(1UL<<w->UserPort->mp_SigBit); if(sig&(1UL<<w->UserPort->mp_SigBit)){ struct IntuiMessage *msg; while((msg=(struct IntuiMessage *)GetMsg(w->UserPort))){ ULONG cls; WORD mx; WORD my; cls=msg->Class; mx=msg->MouseX; my=msg->MouseY; ReplyMsg((struct Message *)msg); if(cls==IDCMP_CLOSEWINDOW) done=1; else if(cls==IDCMP_REFRESHWINDOW) draw_interval_window(w,val); else if(cls==IDCMP_MOUSEBUTTONS){ if(in_rect(mx,my,24,55,34,16)){ if(val>5) val=(UWORD)(val-5); draw_interval_window(w,val); } else if(in_rect(mx,my,66,55,34,16)){ if(val<120) val=(UWORD)(val+5); draw_interval_window(w,val); } else if(in_rect(mx,my,118,55,42,16)){ app->update_interval_min=val; strcpy(app->info_text,"Update interval changed"); draw_panel(parent,app); done=1; } else if(in_rect(mx,my,168,55,54,16)) done=1; } } } }
+    while(!done){ ULONG sig; sig=Wait(1UL<<w->UserPort->mp_SigBit); if(sig&(1UL<<w->UserPort->mp_SigBit)){ struct IntuiMessage *msg; while((msg=(struct IntuiMessage *)GetMsg(w->UserPort))){ ULONG cls; WORD mx; WORD my; cls=msg->Class; mx=msg->MouseX; my=msg->MouseY; ReplyMsg((struct Message *)msg); if(cls==IDCMP_CLOSEWINDOW) done=1; else if(cls==IDCMP_REFRESHWINDOW) draw_interval_window(w,&sg); else if(cls==IDCMP_MOUSEBUTTONS){ if(in_rect(mx,my,48,58,42,16)){ app->update_interval_min=parse_minutes(buf); strcpy(app->info_text,"Update interval changed"); draw_panel(parent,app); done=1; } else if(in_rect(mx,my,102,58,54,16)) done=1; } } } }
     CloseWindow(w);
 }
 LONG gui_run(IssTrackerApp *app)
@@ -172,7 +195,7 @@ LONG gui_run(IssTrackerApp *app)
     SetMenuStrip(win,&menu_settings);
     draw_all(win,app);
     done=0;
-    auto_ticks=0;
+    auto_ticks=((ULONG)app->update_interval_min*AUTO_TICKS_PER_MIN)-1;
     blink_ticks=0;
     while(!done){
         sig=Wait(1UL<<win->UserPort->mp_SigBit);
@@ -195,14 +218,13 @@ LONG gui_run(IssTrackerApp *app)
                     auto_ticks++;
                     blink_ticks++;
                     if(blink_ticks>=5){ blink_ticks=0; app->blink=(UBYTE)!app->blink; if(app->current.valid) draw_iss_blink(win,app); }
-                    if(app->auto_update && auto_ticks>=((ULONG)app->update_interval_min*AUTO_TICKS_PER_MIN)){ auto_ticks=0; update_now(win,app); }
+                    if(auto_ticks>=((ULONG)app->update_interval_min*AUTO_TICKS_PER_MIN)){ auto_ticks=0; update_now(win,app); }
                 } else if(cls==IDCMP_MOUSEBUTTONS){
                     WORD by;
                     button_layout(win,&by);
-                    if(in_rect(mx,my,8,by,BTN_W,BTN_H)) update_now(win,app);
-                    else if(in_rect(mx,my,72,by,BTN_W,BTN_H)){ app->auto_update=(UBYTE)!app->auto_update; auto_ticks=0; draw_panel(win,app); }
-                    else if(in_rect(mx,my,136,by,BTN_W,BTN_H)) info_now(win,app);
-                    else if(in_rect(mx,my,200,by,BTN_W,BTN_H)) done=1;
+                    if(in_rect(mx,my,8,by,BTN_W,BTN_H)){ auto_ticks=0; update_now(win,app); }
+                    else if(in_rect(mx,my,72,by,BTN_W,BTN_H)) info_now(win,app);
+                    else if(in_rect(mx,my,136,by,BTN_W,BTN_H)) done=1;
                 }
             }
         }
